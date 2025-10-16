c34b63870bb4729ae025c467411663609a6d6e04
Sultan Moulana <sulmoulana@gmail.com>
2025-10-16 18:16:24 +0530

    Reorganise project structure for clarity

---
+++ Week-2/Week02Tutorial2/Week02TutorialAdvancedCalculator/Week02TutorialAdvancedCalculator/Week02TutorialAdvancedCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Week02TutorialAdvancedCalculatorApp.swift
+//  Week02TutorialAdvancedCalculator
+//
+//  Created by Sultan  on 16/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct Week02TutorialAdvancedCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
